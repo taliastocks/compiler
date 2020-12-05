ded288337cf57572168eaa06acaf95ffd8f5f165
Talia <928827+taliastocks@users.noreply.github.com>
2020-12-05 08:39:38 -0500

    tests for Namespace (#4)

--- src/compiler/constructs.py
+++ src/compiler/constructs.py
@@ -13,40 +13,59 @@ class Namespace(object):
     """
     _name: typing.Optional[str] = attr.ib(
         default=None,
-        validator=attr.validators.optional(
-            attr.validators.and_(
-                attr.validators.instance_of(str),
-                attr.validators.matches_re(r'^\w+$'),  # Any word characters are allowed,
-                attr.validators.matches_re(r'^\D'),  # except the first character must not be a digit.
-            )
-        )
+        init=False,
+        validator=attr.validators.optional(attr.validators.instance_of(str))
     )
-    _parent: typing.Optional[Namespace] = attr.ib(default=None)
-    _declaration: ConstructBase = attr.ib(default=None, init=False)
+    _parent: typing.Optional[Namespace] = attr.ib(default=None, init=False)
+    _construct: ConstructBase = attr.ib(default=None, init=False)
     _lock: threading.RLock = attr.ib(factory=threading.RLock, init=False)
     _members: typing.MutableMapping[str, Namespace] = attr.ib(factory=dict, init=False)
 
     @property
-    def path(self) -> str:
-        if self._parent is not None:
-            return '.'.join([self._parent.path, self._name])
-        return self._name
-
-    @property
     def name(self) -> str:
+        """Get the name of this namespace.
+        """
         return self._name
 
     @property
-    def parent(self) -> Namespace:
+    def path(self) -> typing.Generator[str, None, None]:
+        """Get the full path of this namespace.
+        """
+        if self._parent is not None:
+            yield from self._parent.path
+        if self._name:
+            yield self._name
+
+    @property
+    def parent(self) -> typing.Optional[Namespace]:
+        """Get the parent of this namespace.
+        """
         return self._parent
 
-    def declare(self, construct: ConstructBase) -> None:
+    @property
+    def members(self) -> typing.Generator[Namespace, None, None]:
+        """Get the sub-namespaces in this namespace.
+        """
+        yield from self._members.values()
+
+    @property
+    def construct(self) -> typing.Optional[ConstructBase]:
+        """Get the construct defined at this path, if any.
+        """
+        return self._construct
+
+    @construct.setter
+    def construct(self, construct: ConstructBase) -> None:
         """Declare a construct to exist at this namespace path.
         """
         with self._lock:
-            if self._declaration is not None:
-                raise ValueError('{} already declared'.format(self.path))
-            self._declaration = construct
+            if self._construct is not None:
+                raise ValueError('{} already declared'.format(list(self.path)))
+            if construct.namespace is not self._parent:
+                raise ValueError('construct.namespace must be the parent namespace')
+            if construct.name != self._name:
+                raise ValueError('construct.name must match this namespace')
+            self._construct = construct
 
     def resolve(self, name: str) -> Namespace:
         """Resolve a namespace by name. The namespace is either a child of the
@@ -58,14 +77,16 @@ class Namespace(object):
                 return self._members[name]
             if self._parent is not None:
                 return self._parent.resolve(name)
-            raise KeyError('no such name {!r} in namespace {}'.format(name, self.path))
+            raise KeyError(name)
 
-    def get(self, name: str) -> Namespace:
+    def __getitem__(self, name: str) -> Namespace:
         """Get a child namespace by name. Creates a new one if one doesn't exist.
         """
         with self._lock:
             if name not in self._members:
-                self._members[name] = Namespace(name, self)
+                self._members[name] = child = Namespace()
+                child._name = name
+                child._parent = self
             return self._members[name]
 
 
@@ -80,7 +101,7 @@ class ConstructBase(object, metaclass=abc.ABCMeta):
     def _declare_with_namespace(self, _, namespace):
         """Declare this construct in the given namespace, or fail.
         """
-        namespace.register(self)
+        namespace[self.name].construct = self
 
 
 @attr.s(frozen=True, slots=True)
