b19e00aaf257de51667e1bdd2e326e1ab0b92f24
Talia Stocks <taliastocks@gmail.com>
2020-12-05 18:07:51 -0500

    construct -> declaration

--- src/compiler/declaration_test.py
+++ src/compiler/declaration_test.py
@@ -1,11 +1,11 @@
 import unittest
 
-from . import constructs
+from . import declaration
 
 
 class NamespaceTestCase(unittest.TestCase):
     def test_name(self):
-        root = constructs.Namespace()
+        root = declaration.Namespace()
         self.assertIsNone(root.name)
 
         child = root['child']
@@ -13,7 +13,7 @@ class NamespaceTestCase(unittest.TestCase):
         self.assertEqual(['child'], list(child.path))
 
     def test_path(self):
-        root = constructs.Namespace()
+        root = declaration.Namespace()
         child = root['child']
         grandchild = child['grandchild']
 
@@ -27,7 +27,7 @@ class NamespaceTestCase(unittest.TestCase):
         )
 
     def test_parent(self):
-        root = constructs.Namespace()
+        root = declaration.Namespace()
         child = root['child']
         grandchild = child['grandchild']
 
@@ -43,7 +43,7 @@ class NamespaceTestCase(unittest.TestCase):
 
     def test_members(self):
         # pylint: disable=pointless-statement
-        root = constructs.Namespace()
+        root = declaration.Namespace()
         root['child1']  # noqa
         root['child2']  # noqa
 
@@ -55,29 +55,29 @@ class NamespaceTestCase(unittest.TestCase):
             list(root.members)
         )
 
-    def test_construct(self):
-        class MyConstruct(constructs.ConstructBase):
+    def test_declaration(self):
+        class MyDeclaration(declaration.DeclarationBase):
             pass
 
-        root = constructs.Namespace()
+        root = declaration.Namespace()
         child = root['child']
 
-        my_construct = MyConstruct('Name', child)
+        my_declaration = MyDeclaration('Name', child)
         self.assertIs(
             child,
-            my_construct.namespace
+            my_declaration.namespace
         )
         self.assertIs(
             'Name',
-            my_construct.name
+            my_declaration.name
         )
         self.assertIs(
-            my_construct,
-            child['Name'].construct
+            my_declaration,
+            child['Name'].declaration
         )
 
         with self.assertRaises(ValueError) as exc:
-            MyConstruct('Name', child)
+            MyDeclaration('Name', child)
 
         self.assertEqual(
             "['child', 'Name'] already declared",
@@ -85,24 +85,24 @@ class NamespaceTestCase(unittest.TestCase):
         )
 
         with self.assertRaises(ValueError) as exc:
-            root['Name'].construct = my_construct
+            root['Name'].declaration = my_declaration
 
         self.assertEqual(
-            'construct.namespace must be the parent namespace',
+            'declaration.namespace must be the parent namespace',
             str(exc.exception)
         )
 
         with self.assertRaises(ValueError) as exc:
-            child['OtherName'].construct = my_construct
+            child['OtherName'].declaration = my_declaration
 
         self.assertEqual(
-            'construct.name must match this namespace',
+            'declaration.name must match this namespace',
             str(exc.exception)
         )
 
     def test_resolve(self):
         # pylint: disable=pointless-statement
-        root = constructs.Namespace()
+        root = declaration.Namespace()
         root['child1']['grandchild1']  # noqa
         root['child1']['conflict']  # noqa
         root['child2']['grandchild1']  # noqa
