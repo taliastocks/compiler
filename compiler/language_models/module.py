c987909380076fb6936a412d0f1d171ba40ccd67
Talia Stocks <taliastocks@gmail.com>
2021-02-14 15:36:27 -0500

    parse module

--- compiler/language_models/module.py
+++ compiler/language_models/module.py
@@ -6,81 +6,57 @@ import attr
 import immutabledict
 
 from . import (
-    class_ as class_module,
     declarable as declarable_module,
-    expression,
-    function as function_module,
-    reference as path_module,
+    statement as statement_module,
 )
 from .. import parser as parser_module
 
 
 @attr.s(frozen=True, slots=True)
 class Module(parser_module.Symbol):
-    imports: typing.Collection[Import] = attr.ib(converter=frozenset, default=(), repr=False)
-    functions: typing.Collection[function_module.Function] = attr.ib(converter=frozenset, default=(), repr=False)
-    classes: typing.Collection[class_module.Class] = attr.ib(converter=frozenset, default=(), repr=False)
-    variables: typing.Collection[expression.Variable] = attr.ib(converter=frozenset, default=(), repr=False)
-
+    statements: typing.Sequence[statement_module.Statement] = attr.ib(converter=tuple)
     globals: typing.Mapping[declarable_module.Declarable] = attr.ib(converter=immutabledict.immutabledict,
                                                                     init=False,
                                                                     repr=False)
 
     @classmethod
     def from_string(cls, code: str):
-        pass
+        cursor = parser_module.Cursor(code.splitlines())
+
+        return cls.parse(cursor).last_symbol
 
     @classmethod
     def parse(cls, cursor: parser_module.Cursor):
-        imports = []
-        functions = []
-        classes = []
-        variables = []
+        statements: list[statement_module.Statement] = []
 
         while not isinstance(cursor.last_symbol, parser_module.EndFile):
             cursor = cursor.parse_one_symbol([
                 parser_module.EndFile,
-                Import,
-                function_module.Function,
-                class_module.Class,
-                expression.Variable,
+                statement_module.Statement,
             ])
-            if isinstance(cursor.last_symbol, Import):
-                imports.append(cursor.last_symbol)
-            elif isinstance(cursor.last_symbol, function_module.Function):
-                functions.append(cursor.last_symbol)
-            elif isinstance(cursor.last_symbol, class_module.Class):
-                classes.append(cursor.last_symbol)
-            elif isinstance(cursor.last_symbol, expression.Variable):
-                variables.append(cursor.last_symbol)
+            if isinstance(cursor.last_symbol, statement_module.Statement):
+                statements.append(cursor.last_symbol)
+            else:  # EndFile
+                break
 
         return cursor.new_from_symbol(cls(
-            imports=imports,
-            functions=functions,
-            classes=classes,
-            variables=variables,
+            cursor=cursor,
+            statements=statements,
         ))
 
     @globals.default
     def _init_globals(self):
         global_declarations = {}
 
-        for declarables in [self.imports, self.functions, self.classes, self.variables]:
-            for declarable in declarables:
-                if declarable.name in global_declarations:
-                    raise ValueError('{!r} cannot be declared with the same name as {!r}'.format(
-                        declarable,
-                        global_declarations[declarable.name]
-                    ))
-                global_declarations[declarable.name] = declarable
+        for statement in self.statements:
+            for receiver in statement.receivers:
+                if isinstance(receiver, declarable_module.Declarable):
+                    declarable: declarable_module.Declarable = receiver
+                    if declarable.name in global_declarations:
+                        raise ValueError('{!r} cannot be declared with the same name as {!r}'.format(
+                            declarable,
+                            global_declarations[declarable.name]
+                        ))
+                    global_declarations[declarable.name] = declarable
 
         return global_declarations
-
-
-@attr.s(frozen=True, slots=True)
-class Import(declarable_module.Declarable, parser_module.Symbol):
-    path: path_module.ModuleReference = attr.ib()
-
-    @classmethod
-    def parse(cls, cursor):
-        pass  # Placeholder until I get around to writing a real implementation.
--- compiler/language_models/module_test.py
+++ compiler/language_models/module_test.py
@@ -1,20 +1,85 @@
+import textwrap
 import unittest
 
-from . import module, function, class_, reference, expression
+from . import module, function, class_, expression, statement
+from .. import parser as parser_module
 
 
 class ModuleTestCase(unittest.TestCase):
+    def test_from_string(self):
+        self.assertEqual(
+            module.Module.from_string(textwrap.dedent(
+                '''\
+                import foo
+
+                def bar():
+                    hello
+                '''
+            )),
+            module.Module([
+                statement.Declaration(
+                    statement.Import('foo', ['foo'])
+                ),
+                statement.Declaration(
+                    function.Function(
+                        name='bar',
+                        body=statement.Block([
+                            statement.Declaration(
+                                expression.Variable('hello')
+                            )
+                        ]),
+                    )
+                ),
+            ])
+        )
+
+    def test_parse(self):
+        self.assertEqual(
+            module.Module.parse(
+                parser_module.Cursor([
+                    'import foo',
+                    'def bar():',
+                    '    hello',
+                ])
+            ).last_symbol,
+            module.Module([
+                statement.Declaration(
+                    statement.Import('foo', ['foo'])
+                ),
+                statement.Declaration(
+                    function.Function(
+                        name='bar',
+                        body=statement.Block([
+                            statement.Declaration(
+                                expression.Variable('hello')
+                            )
+                        ]),
+                    )
+                ),
+            ])
+        )
+
     def test_init_globals(self):
         my_module = module.Module(
-            imports=[module.Import('foo', reference.ModuleReference('path.to.foo'))],
-            functions=[function.Function('bar')],
-            classes=[class_.Class('Baz')],
-            variables=[expression.Variable('bip')],
+            statements=[
+                statement.Declaration(
+                    statement.Import('foo', ['foo'])
+                ),
+                statement.Declaration(
+                    function.Function('bar')
+                ),
+                statement.Declaration(
+                    class_.Class('Baz')
+                ),
+                statement.Declaration(
+                    expression.Variable('bip')
+                ),
+            ],
         )
 
         self.assertEqual(
             {
-                'foo': module.Import('foo', reference.ModuleReference('path.to.foo')),
+                'foo': statement.Import('foo', ['foo']),
                 'bar': function.Function('bar'),
                 'Baz': class_.Class('Baz'),
                 'bip': expression.Variable('bip'),
@@ -25,6 +90,12 @@ class ModuleTestCase(unittest.TestCase):
         with self.assertRaisesRegex(ValueError, r"Variable\(name='foo'\) cannot be declared with the same "
                                                 r"name as Function\(name='foo'\)"):
             module.Module(
-                functions=[function.Function('foo')],
-                variables=[expression.Variable('foo')],
+                statements=[
+                    statement.Declaration(
+                        function.Function('foo')
+                    ),
+                    statement.Declaration(
+                        expression.Variable('foo')
+                    ),
+                ],
             )
